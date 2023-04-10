79c6666e4c8c4cbfeee071703af4c283863377aa
Eugene Roy <box.rz.box@gmail.com>
2023-04-10 11:34:03 +0300

    lesson2

---
+++ lesson2/variables.py
@@ -0,0 +1,18 @@
+# 1. Naming
+# Can use: letters, numbers, underscore
+# Cant use: anything else
+
+# Can start with letter or underscore
+
+welcome1 = "********************"
+welcome2 = "*                  *"
+welcome3 = "*     WELCOME!     *"
+
+print('')
+print(welcome1)
+print(welcome2)
+print(welcome3)
+print(welcome2)
+print(welcome1)
+
+# Exsess spaces matters!
